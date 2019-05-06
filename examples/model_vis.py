c8878cf2e8dce4aeb94dd35a6593ab594226a653
inkyusa <enddl22@gmail.com>
2019-05-06 16:59:08 +0200

    first bouncing ball test model
    head to examples folder and python ./model_vis --m
    ../xmls/assets/bouncing_ball.xml

---
+++ examples/model_vis.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Example of visualizing xml model
+"""
+from mujoco_py import load_model_from_path, MjSim, MjViewer
+import math
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Description of your program')
+parser.add_argument('-m','--model', help='Description for foo argument', required=True)
+args = vars(parser.parse_args())
+
+print("args=",args['model'])
+
+model = load_model_from_path(args['model'])
+sim = MjSim(model)
+viewer = MjViewer(sim)
+
+viewer.cam.trackbodyid = 1
+viewer.cam.distance = model.stat.extent * 5.0
+#viewer.cam.lookat[2] += .8
+#viewer.cam.elevation = -20
+
+t = 0
+while True:
+    # sim.data.ctrl[0] = 4.1
+    # sim.data.ctrl[1] = 0
+    # sim.data.ctrl[2] = 0
+    # sim.data.ctrl[3] = 0
+    
+    t += 1
+    sim.step()
+    viewer.render()
+    if t > 100 and os.getenv('TESTING') is not None:
+        break
